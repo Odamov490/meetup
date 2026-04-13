cb60a610057cddab40a8d98fe9fda14e45e5590d
Odamov490 <Piskentems@gmail.com>
2026-04-13 19:23:28 +0500

    s

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,13 @@
 // O'z Firebase loyihangiz ma'lumotlarini shu yerga kiriting
 // https://console.firebase.google.com → Project Settings → Web app
 
+// Firebase konfiguratsiyasi
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCkjaxRC2q3V1y58OvraEce9aDC_ptcLuE",
+  authDomain: "meetup-d67f6.firebaseapp.com",
+  projectId: "meetup-d67f6",
+  storageBucket: "meetup-d67f6.firebasestorage.app",
+  messagingSenderId: "942720343437",
+  appId: "1:942720343437:web:12d42e1360b65eaa23b1c4",
+  measurementId: "G-FNK8X9T5JC"
 };
